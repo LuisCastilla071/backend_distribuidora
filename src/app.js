e61344d28e88c16c3c759b763a0bb4d79a354ee3
LuisCastilla071 <luisjaviercastillasuare@gmail.com>
2025-03-31 22:27:52 -0600

    Se agrega compras y detalle de compras

--- src/app.js
+++ src/app.js
@@ -3,6 +3,9 @@ import cors from 'cors';
 import rutasClientes from './routes/clientes.routes.js';
 import rutasUsuarios from './routes/usuarios.routes.js';
 import rutasProductos from './routes/productos.routes.js';
+import rutasCategorias from './routes/categorias.routes.js';
+import rutasVentas from './routes/ventas.routes.js';
+import rutasCompras from './routes/compras.routes.js';
 
 const app = express();
 
@@ -17,6 +20,9 @@ app.use(express.json());
 app.use('/api', rutasClientes);
 app.use('/api', rutasUsuarios);
 app.use('/api', rutasProductos);
+app.use('/api', rutasCategorias);
+app.use('/api', rutasVentas);
+app.use('/api', rutasCompras);
 
 // Manejo de rutas no encontradas
 app.use((req, res, next) => {

a17a45aebeeb721776f2970bca1cbe92edec0bb0
LuisCastilla071 <luisjaviercastillasuare@gmail.com>
2025-04-03 21:17:08 -0600

    Se agrega la funcionalidad de agregar en producto

--- src/routes/productos.routes.js
+++ src/routes/productos.routes.js
@@ -1,7 +1,10 @@
 import { Router } from 'express';
-import {  obtenerProductos, obtenerProducto } from '../controllers/productos.controller.js';
+import {  registrarProducto, obtenerProductos, obtenerProducto } from '../controllers/productos.controller.js';
 
 const router = Router();
+
+// Ruta para obtener un produto por su ID
+router.post('/registrarproducto', registrarProducto);
 
 // Ruta para obtener todos los productos
 router.get('/productos', obtenerProductos);
